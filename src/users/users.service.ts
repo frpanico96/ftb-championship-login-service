55d89217893060e7b21b57fd5324644f8e0d39de
francescopanico <frpanico96@gmail.com>
2024-06-15 18:47:11 +0200

    [FTB] First Commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = any; //Todo: add user interface
+
+@Injectable()
+export class UsersService {
+  // Replace with Monoose injection
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'john',
+      password: 'changeme',
+    },
+    {
+      userId: 2,
+      username: 'maria',
+      password: 'guess',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
